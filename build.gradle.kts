60ce4304232880c7617d6b10b116242d607c14d2
Thibaut Lefrançois <thibaut.lfr@gmail.com>
2024-05-07 12:31:19 +0200

    init[global]: added dependencies for ORM, Logback and DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.exposed", "exposed-core", "0.50.0")
+    implementation("org.jetbrains.exposed", "exposed-dao", "0.50.0")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.50.0")
+    implementation("org.xerial:sqlite-jdbc:3.44.1.0")
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.1")
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 tasks.test {
